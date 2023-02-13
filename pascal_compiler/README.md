463c73b6936c84c5ed21557ab0ffc0c62a311430
hkzwhn <liuzhaocen@sina.com>
2023-02-12 23:57:01 -0500

    run time error

---
+++ pascal_compiler/README.md
@@ -0,0 +1,4 @@
+chapter 6:  
+&nbsp;&nbsp; analysis the AST to calculation the parser  
+&nbsp;&nbsp; mean the run-time parser.
+
---
+++ pascal_compiler/src/wci/backend/interpreter/RuntimeErrorHandler.java
@@ -0,0 +1,11 @@
+package wci.backend.interpreter;
+
+/**
+ * @Author zhaocenliu
+ * @create 2023/2/12 11:45 PM
+ */
+public class RuntimeErrorHandler {
+    private static final int MAX_ERRORS = 5;
+    private static int errorCount = 0;
+    
+}
