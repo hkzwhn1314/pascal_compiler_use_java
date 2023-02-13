82e82375a2850f87a848747c9a25271f138c0bb0
hkzwhn <liuzhaocen@sina.com>
2023-02-13 00:04:43 -0500

    run time error

--- pascal_compiler/src/wci/backend/interpreter/RuntimeErrorHandler.java
+++ pascal_compiler/src/wci/backend/interpreter/RuntimeErrorHandler.java
@@ -1,11 +1,39 @@
 package wci.backend.interpreter;
 
+import wci.backend.Backend;
+import wci.intermediate.ICodeNode;
+import wci.message.Message;
+
+import static wci.intermediate.icodeimpl.ICodeKeyImpl.LINE;
+import static wci.message.MessageType.RUNTIME_ERROR;
+
 /**
+ * @Define define the run-time error
  * @Author zhaocenliu
  * @create 2023/2/12 11:45 PM
  */
 public class RuntimeErrorHandler {
     private static final int MAX_ERRORS = 5;
     private static int errorCount = 0;
-    
+
+    public void flag(ICodeNode node, RuntimeErrorCode errorCode,
+                     Backend backend) {
+        String lineNumber = null;
+        while ((node != null) && (node.getAttribute(LINE) == null)) {
+            node = node.getParent();
+        }
+
+        // Notify the interpreter&apos;s listeners.
+        backend.sendMessage(
+                new Message(RUNTIME_ERROR,
+                        new Object[]{errorCode.toString(),
+                                (Integer) node.getAttribute(LINE)}));
+
+        if (++errorCount > MAX_ERRORS) {
+            System.out.println("*** ABORTED AFTER TOO MANY RUNTIME ERRORS.");
+            System.exit(-1);
+        }
+
+
+    }
 }
