e89ced465407fbfc17d5b1f79f4e7ee0e6bfcfda
hkzwhn <liuzhaocen@sina.com>
2023-02-17 00:09:21 -0500

    while

--- pascal_compiler/src/wci/frontend/pascal/parsers/RepeatStatementParser.java
+++ pascal_compiler/src/wci/frontend/pascal/parsers/RepeatStatementParser.java
@@ -2,7 +2,13 @@ package wci.frontend.pascal.parsers;
 
 import wci.frontend.Token;
 import wci.frontend.pascal.PascalParserTD;
+import wci.intermediate.ICodeFactory;
 import wci.intermediate.ICodeNode;
+
+import static wci.frontend.pascal.PascalErrorCode.MISSING_UNTIL;
+import static wci.frontend.pascal.PascalTokenType.UNTIL;
+import static wci.intermediate.icodeimpl.ICodeNodeTypeImpl.LOOP;
+import static wci.intermediate.icodeimpl.ICodeNodeTypeImpl.TEST;
 
 /**
  * @Author zhaocenliu
@@ -17,6 +23,17 @@ public class RepeatStatementParser extends StatementParser {
 
     // 语法形式:REPEAT j := i;k := i;UNTIL i <= j
     public ICodeNode parse(Token token) throws Exception {
+        token = nextToken(); // consume the REPEAT
+        // Create the LOOP and TEST nodes.
+        ICodeNode loopNode = ICodeFactory.createICodeNode(LOOP);
+        ICodeNode testNode = ICodeFactory.createICodeNode(TEST);
+        StatementParser statementParser = new StatementParser(this);
+        statementParser.parseList(token, loopNode, UNTIL, MISSING_UNTIL);
+        token = currentToken();
+
+        ExpressionParser expressionParser = new ExpressionParser(this);
+        testNode.addChild(expressionParser.parse(token));
+        loopNode.addChild(testNode);
         return null;
     }
 }
--- pascal_compiler/src/wci/frontend/pascal/parsers/WhileStatementParser.java
+++ pascal_compiler/src/wci/frontend/pascal/parsers/WhileStatementParser.java
@@ -2,7 +2,12 @@ package wci.frontend.pascal.parsers;
 
 import wci.frontend.Token;
 import wci.frontend.pascal.PascalParserTD;
+import wci.frontend.pascal.PascalTokenType;
 import wci.intermediate.ICodeNode;
+
+import java.util.EnumSet;
+
+import static wci.frontend.pascal.PascalTokenType.DO;
 
 /**
  * @Author zhaocenliu
@@ -18,6 +23,15 @@ public class WhileStatementParser extends StatementParser {
      */
     public WhileStatementParser(PascalParserTD parent) {
         super(parent);
+    }
+
+    // Synchronization set for DO.
+    private static final EnumSet<PascalTokenType> DO_SET =
+            StatementParser.STMT_START_SET.clone();
+
+    static {
+        DO_SET.add(DO);
+        DO_SET.addAll(StatementParser.STMT_FOLLOW_SET);
     }
 
     public ICodeNode parse(Token token) throws Exception {
