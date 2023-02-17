641d0f8e33e8e4b2b68bceebe9addd4da21cd56f
hkzwhn <liuzhaocen@sina.com>
2023-02-17 18:57:17 -0500

    while parser finish

--- pascal_compiler/src/wci/frontend/pascal/parsers/WhileStatementParser.java
+++ pascal_compiler/src/wci/frontend/pascal/parsers/WhileStatementParser.java
@@ -10,6 +10,7 @@ import wci.intermediate.icodeimpl.ICodeNodeTypeImpl;
 
 import java.util.EnumSet;
 
+import static wci.frontend.pascal.PascalErrorCode.MISSING_DO;
 import static wci.frontend.pascal.PascalTokenType.DO;
 import static wci.intermediate.icodeimpl.ICodeNodeTypeImpl.LOOP;
 
@@ -47,6 +48,32 @@ public class WhileStatementParser extends StatementParser {
         ICodeNode loopNode = ICodeFactory.createICodeNode(LOOP);
         ICodeNode breakNode = ICodeFactory.createICodeNode(TEST);
         ICodeNode notNode = ICodeFactory.createICodeNode(ICodeNodeTypeImpl.NOT);
-        return null;
+
+        // The LOOP node adopts the TEST node as its first child.
+        // The TEST node adopts the NOT node as its only child.
+        loopNode.addChild(breakNode);
+        breakNode.addChild(notNode);
+
+        // Parse the expression.
+        // The NOT node adopts the expression subtree as its only child.
+        ExpressionParser expressionParser = new ExpressionParser(this);
+        notNode.addChild(expressionParser.parse(token));
+
+        // Synchronize at the DO.
+        token = synchronize(DO_SET);
+        // Synchronize at the DO.
+        token = synchronize(DO_SET);
+        if (token.getType() == DO) {
+            token = nextToken(); // consume the DO
+        } else {
+            errorHandler.flag(token, MISSING_DO, this);
+        }
+
+        // Parse the statement.
+        // The LOOP node adopts the statement subtree as its second child.
+        StatementParser statementParser = new StatementParser(this);
+        loopNode.addChild(statementParser.parse(token));
+        
+        return loopNode;
     }
 }
