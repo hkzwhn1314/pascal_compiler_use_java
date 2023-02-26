58f5e8347944f5611225af2508ab0ffbac560725
hkzwhn <liuzhaocen@sina.com>
2023-02-25 23:06:28 -0500

    execute

--- pascal_compiler/src/wci/backend/interpreter/executors/SelectExecutor.java
+++ pascal_compiler/src/wci/backend/interpreter/executors/SelectExecutor.java
@@ -3,6 +3,8 @@ package wci.backend.interpreter.executors;
 import wci.backend.interpreter.Executor;
 import wci.intermediate.ICodeNode;
 
+import java.util.ArrayList;
+
 /**
  * @Author zhaocenliu
  * @create 2023/2/25 7:55 PM
@@ -13,6 +15,26 @@ public class SelectExecutor extends StatementExecutor {
     }
 
     public Object execute(ICodeNode node) {
+        // Get the SELECT node&apos;s children.
+        ArrayList<ICodeNode> selectChildren = node.getChildren();
+        ICodeNode exprNode = selectChildren.get(0);
+        // Evaluate the SELECT expression.
+        ExpressionExecutor expressionExecutor = new ExpressionExecutor(this);
+        Object selectValue = expressionExecutor.execute(exprNode);
+        // Attempt to select a SELECT_BRANCH.
+        ICodeNode selectedBranchNode = searchBranches(selectValue,
+                selectChildren);
+        // If there was a selection, execute the SELECT_BRANCH&apos;s statement.
+        if (selectedBranchNode != null) {
+            ICodeNode stmtNode = selectedBranchNode.getChildren().get(1);
+            StatementExecutor statementExecutor = new StatementExecutor(this);
+            statementExecutor.execute(stmtNode);
+        }
+        ++executionCount; // count the SELECT statement itself
+        return null;
+    }
+    //TODO
+    private ICodeNode searchBranches(Object selectValue, ArrayList<ICodeNode> selectChildren) {
         return null;
     }
 }
